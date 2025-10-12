129059e620d7c9f5df7ea6fed7885f73158c8128
Your Name <your.email@example.com>
2025-10-12 20:30:08 +0200

    Refactor frontend files: remove unused HTMLResponse import, restructure script.js for better organization, and ensure proper HTML structure in index.html

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,21 @@
-const userId = 1;
-ws = new WebSocket(`ws://localhost:8000/ws/${userId}`)
+window.addEventListener("DOMContentLoaded", () => {
+        /** @type {HTMLInputElement} */
+    const input = document.getElementById("input");
+    const button = document.getElementById("btn");
+    const messagesDiv = document.getElementById("messages");
 
+    const userId = 1;
+    const ws = new WebSocket(`ws://localhost:8000/ws/${userId}`);
+
+    const ul = document.createElement("ul");
+    messagesDiv.append(ul);
+
+    button.addEventListener("click", () => {
+        const value = input.value;
+        ws.send(value);
+        const li = document.createElement("li");
+        li.textContent = value;
+        ul.append(li);
+        value = "";
+    });
+});
